f0e55f85b1f29591b6d68e5a1fe0f59bb4520a4d
Alex Slater <alexjslater2@gmail.com>
2020-09-30 08:51:37 +0100

    multiplayer, weapon, and shield additions with menus

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -5,6 +5,7 @@ var BASE_MOVEMENT_WEIGHT = .003
 var BASE_MOVEMENT_SPEED = 16*16
 var BASE_BOOST_SPEED = 1000 # fast thrust
 var BASE_SHIELD_DISTANCE = 32
+var BASE_WEAPON_DISTANCE = 20
 
 # movement vars
 var speed_multiplier = 2
@@ -19,16 +20,41 @@ var t_amount_boost = 1
 var boosting = false
 var shielding = false
 
+# onready vars
 onready var body = $Sprite
 onready var shield = $Shield
+onready var weapon = $Weapon
+onready var local_mouse_pos = get_local_mouse_position()
+onready var camera = $PlayerCamera
+
+# network vars
+puppet var puppet_position = Vector2()
+puppet var puppet_mouse_vector = Vector2()
+puppet var puppet_shield_vector = Vector2()
 
 func _physics_process(_delta):
-	handle_input()
+	update_state()
+	if(is_network_master()):
+		handle_input()
+		update_objects()
+	else:
+		update_network_variables()
+	
 	calculate_movement()
 	apply_movement()
-	update_state()
 	
+func update_network_variables():
+	position = puppet_position
+	weapon.position = puppet_mouse_vector.normalized() * BASE_WEAPON_DISTANCE
+	weapon.rotation = puppet_mouse_vector.angle() + PI/2
+	shield.position = puppet_mouse_vector.normalized() * BASE_SHIELD_DISTANCE
+	shield.rotation = puppet_mouse_vector.angle() + PI/2
+
 func handle_input():
+	# update network position
+	rset_unreliable('puppet_position', position)
+	rset_unreliable('puppet_mouse_vector', local_mouse_pos)
+
 	# get input and set vector with movement speed
 	input_velocity = Vector2.ZERO
 	input_velocity.x = -int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right"))
@@ -41,14 +67,27 @@ func handle_input():
 	
 	# if boosting
 	if(Input.is_action_just_pressed("ui_click_left")):
-		input_move_vector = get_local_mouse_position().normalized() * BASE_BOOST_SPEED
+		input_move_vector = local_mouse_pos.normalized() * BASE_BOOST_SPEED
 		boosting = true
 
 	if(Input.is_action_pressed("ui_click_right")):
-		shield.position = get_local_mouse_position().normalized() * BASE_SHIELD_DISTANCE
-		shield.show()
-		shielding = true
-	
+		show_shield()
+
+	if(Input.is_action_just_released("ui_click_right")):
+		hide_shield()
+
+func show_shield():
+	shield.position = local_mouse_pos.normalized() * BASE_SHIELD_DISTANCE
+	shield.rotation = local_mouse_pos.angle() + PI/2 # flip 90 deg
+	weapon.hide()
+	shield.show()
+	shielding = true
+
+func hide_shield():
+	shielding = false
+	weapon.show()
+	shield.hide()
+
 func calculate_movement():
 	# interpolate velocity towards desired using move weight
 	if(input_move_vector == Vector2.ZERO):
@@ -57,11 +96,13 @@ func calculate_movement():
 
 	velocity = move_and_slide(velocity)
 
+func update_objects():
+	weapon.position = local_mouse_pos.normalized() * BASE_WEAPON_DISTANCE
+	weapon.rotation = local_mouse_pos.angle() + PI/2
+
 func update_state():
+	local_mouse_pos = get_local_mouse_position()
 	boosting = false
-	if(Input.is_action_just_released("ui_click_right")):
-		shielding = false
-		shield.hide()
 
 func apply_movement():
 	# with bounce logic
---
+++ LevelManagers/networking.gd
@@ -0,0 +1,94 @@
+extends Node
+
+# nodes
+onready var SERVER_IP = $Columns/InputSection/Rows/IPInput
+onready var SERVER_PORT = $Columns/InputSection/Rows/PortInput
+onready var NAME = $Columns/InputSection/Rows/NameInput
+
+# network variables
+var player_info = {}
+var my_info = { name = 'slater-duud' }
+
+func _ready():
+	get_tree().connect("network_peer_connected", self, "_player_connected")
+	get_tree().connect("network_peer_disconnected", self, "_player_disconnected")
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connection_failed", self, "_connected_fail")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
+	my_info.name = NAME
+
+func start_server():
+	var peer = NetworkedMultiplayerENet.new()
+	peer.create_server(int(SERVER_PORT.get_text()), 10)
+	get_tree().network_peer = peer
+
+func start_client():
+	var peer = NetworkedMultiplayerENet.new()
+	print("Attempting connection to: ", SERVER_IP.get_text(), ":", SERVER_PORT.get_text())
+	peer.create_client(SERVER_IP.get_text(), int(SERVER_PORT.get_text()))
+	get_tree().network_peer = peer
+
+func stop_networking():
+	get_tree().network_peer = null
+
+func start_game(): 
+	rpc("pre_configure_game")
+
+func _connected_ok():
+	print('Connected')
+
+func _server_disconnected():
+	pass
+
+func _connected_fail():
+	print('Connection Failed')
+	stop_networking()
+
+func _player_connected(id):
+	print(id)
+	rpc_id(id, "register_player", my_info)
+
+func _player_disconnected(id):
+	player_info.erase(id)
+
+remotesync func register_player(info):
+	var id = get_tree().get_rpc_sender_id()
+	player_info[id] = info
+
+remotesync func pre_configure_game():
+	get_tree().paused = true
+	var selfPeerID = get_tree().get_network_unique_id()
+	# load world
+	var level = load('res://Levels/TestPlayground.tscn').instance()
+	get_node("/root").add_child(level)
+
+	# load self player
+	var my_player = preload("res://Characters/Player.tscn").instance()
+	my_player.name = str(selfPeerID)
+	my_player.set_network_master(selfPeerID)
+	get_node('/root/TestPlayground').add_child(my_player)
+
+	# load other players
+	for p in player_info:
+		var player = preload("res://Characters/Player.tscn").instance()
+		player.get_child(0).queue_free()
+		player.name = str(p)
+		player.set_network_master(p)
+		get_node('/root/TestPlayground').add_child(player)
+		
+	rpc_id(1, "done_preconfig")
+
+var players_done = []
+
+remotesync func done_preconfig():
+	var who = get_tree().get_rpc_sender_id()
+	players_done.append(who)
+
+	if(players_done.size() - 1 == player_info.size()):
+		rpc("post_configure_game")
+
+remotesync func post_configure_game():
+	if(get_tree().get_rpc_sender_id() == 1):
+		get_tree().paused = false
+	# delete menu
+	$Columns.hide()
